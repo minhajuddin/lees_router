ba648f4e821919431a4b03d501d7782e20428379
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-10-29 18:56:23 +0530

    basic wireup working

---
+++ boot.cpp
@@ -0,0 +1,22 @@
+/*
+ * Lee's router
+ *
+ * Author: Khaja Minhajuddin
+ */
+
+#include<iostream>
+#include "gridpoint.cpp"
+#include "node.cpp"
+#include "queue.cpp"
+#include "router.cpp"
+
+using namespace std;
+
+int main(){
+  int rows;
+  cout << "Please enter the size of the grid:" << endl;
+  cin >> rows;
+  Router *router = new Router(rows, rows);
+  router->Info();
+  return 0;
+}
--- router.cpp
+++ router.cpp
@@ -1,16 +1,24 @@
-/*
- * Lee's router
- *
- * Author: Khaja Minhajuddin
- */
-
 #include<iostream>
-#include "gridpoint.cpp"
-#include "node.cpp"
-#include "queue.cpp"
-
 using namespace std;
+class Router
+{
+  public:
+    Router(int rows, int cols);
+    void Info();
+  private:
+    int rows;
+    int cols;
+};
 
-int main(){
-  Queue *q = new Queue();
+Router::Router(int r, int c)
+{
+  rows = r;
+  cols = c;
 }
+
+void Router::Info(){
+  cout << "INFO:" << endl;
+  cout << "Rows:" << rows << endl
+       << "Cols:" << cols << endl;
+}
+
